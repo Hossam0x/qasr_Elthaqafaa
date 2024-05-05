9c937528b937a8a76680f406f1159dfbd7f6ace8
Reem-Magdyy <rmagdy.2233@gmail.com>
2024-05-05 20:12:37 +0200

    ...

--- handelers/handelsign.php
+++ handelers/handelsign.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-//include 'Reservationsdata.php';
 include '../database.php';
 
 // Check if the form is submitted
@@ -10,45 +9,44 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $firstname = $_POST['fname'];
     $lastname = $_POST['lname'];
     $phone = $_POST['phone'];
-    $card=$_POST['cardnum'];
-    $type='test';
-    $date='test';
-    $price='test';
-}
-    $id=$_GET['id'];
-    $sqll="SELECT * from storedata where id='$id'";
+    $card = $_POST['cardnum']; 
+    $type = 'test';
+    $date = 'test';
+    $price = 'test';
+    $id = $_GET['id'];
+
+    // Retrieve data from 'storedata' table
+    $sqll = "SELECT * FROM storedata WHERE id='$id'";
     $result = mysqli_query($con, $sqll);
     if (mysqli_num_rows($result) > 0) {
         $row = mysqli_fetch_assoc($result);
-        $type=$row['type'];
-        $date=$row['date'];
-        $price=$row['price'];
-        $name=$row['name'];
+        $type = $row['type'];
+        $date = $row['date'];
+        $price = $row['price'];
+        $name = $row['name'];
     }
 
+    // Prepare SQL query
+    $sql = "INSERT INTO reservations (user_email, type, name, date, phone, first_name, last_name, price, cardnumber) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-$sql = "INSERT INTO reservations (user_email,type,name, date,phone,first_name,last_name,price) VALUES (?,?,?, ?,?,?,?,?)";
+    // Prepare and bind parameters
+    $stmt = $con->prepare($sql);
+    $stmt->bind_param("sssssssss", $email, $type, $name, $date, $phone, $firstname, $lastname, $price, $card);
 
-    
-// Prepare and bind parameters
-$stmt = $con->prepare($sql);
-$stmt->bind_param("ssssssss",$email, $type,$name, $date,$phone,$firstname,$lastname,$price);
+    // Execute the statement
+    if ($stmt->execute()) {
+        // Redirect to success page
+        $_SESSION['create'] = "ticket added successfully.";
+        header("Location: ../sign.php?id=" . $id);
+        exit();
+    } else {
+        echo "Error: " . $sql . "<br>" . $con->error;
+    }
 
-// Execute the statement
-if ($stmt->execute()) {
-    // Redirect to success page
-    $_SESSION['create'] = "ticket added successfully.";
-    header("Location: ../sign.php?id=" . $id);
-    exit();
-
-} else {
-    echo "Error: " . $sql . "<br>" . $con->error;
+    // Close statement
+    $stmt->close();
 }
 
-// Close statement
-$stmt->close();
-
-// Close conection
+// Close connection
 $con->close();
 ?>
-
